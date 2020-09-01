ae993281f0226d74e6580895c3ea5f3149b99e7a
Emeke <emeke.opute@outlook.com>
2020-09-01 12:23:53 -0500

    update welcome address

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const server = express();
 
 server.get("/", (req, res) => {
-  res.send("Welcome home");
+  res.send("Welcome home via github");
 });
 
 let PORT = process.env.PORT ? process.env.PORT : 5000;
